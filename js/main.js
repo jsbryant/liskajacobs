d31aca5cceb2e9840a0b2325462dc2482350b049
Jordan Bryant <jsbrymail@gmail.com>
2017-09-23 14:17:06 -0700

    Added js

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// SMOOTH SCROLL SCRIPT
+
+$(document).ready(function(){
+  
+  $("a").on('click', function(event) {
+    
+    if (this.hash !== "") {
+      
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+
+
+// TOP BUTTON SCRIPT
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
