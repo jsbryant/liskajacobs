498c717d7c188bc72e2ac768ad04a97730c44e0a
Jordan Bryant <jsbrymail@gmail.com>
2017-12-08 12:41:57 -0800

    Updated 'return to top' btn

--- js/main.js
+++ js/main.js
@@ -1,48 +1,45 @@
-// SMOOTH SCROLL SCRIPT
-
+// smooth scrolling
 $(document).ready(function(){
-  
+  // Add smooth scrolling to all links
   $("a").on('click', function(event) {
-    
+
+    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      
+      // Prevent default anchor click behavior
       event.preventDefault();
 
+      // Store hash
       var hash = this.hash;
 
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
    
+        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    }
+    } // End if
   });
 });
 
-
-
-// TOP BUTTON SCRIPT
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("topBtn").style.display = "block";
+// scroll to top (Thanks to Ross Dallaire for Codepen ref:https://codepen.io/rdallaire/)
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
     } else {
-        document.getElementById("topBtn").style.display = "none";
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
     }
-}
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
 
 
-function topFunction() {
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-    document.documentElement.scrollTop = 0; // For IE and Firefox
-}
-
-
-// LAZY LOAD SCRIPT
-
+// lazy load script
 [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
   img.setAttribute('src', img.getAttribute('data-src'));
   img.onload = function() {
